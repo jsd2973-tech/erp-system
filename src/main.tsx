b32d26e0531bbef07aeb132255dafe97491706ac
jsd2973-tech <jsd2973@gmail.com>
2026-09-09 13:26:41 +0900

    style: load final dispatch tuning

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import DispatchAuthGate from './features/dispatch/DispatchAuthGate.tsx'
 import './features/dispatch/dispatch-polish.css'
 import './features/dispatch/dispatch-modern.css'
+import './features/dispatch/dispatch-final-tune.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
