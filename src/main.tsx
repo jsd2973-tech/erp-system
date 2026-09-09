b58aa32a78966983cda11ce4505340d0e462d15f
jsd2973-tech <jsd2973@gmail.com>
2026-09-09 13:38:36 +0900

    style: enable unified dispatch suite skin

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import DispatchAuthGate from './features/dispatch/DispatchAuthGate.tsx'
 import './features/dispatch/dispatch-polish.css'
 import './features/dispatch/dispatch-modern.css'
 import './features/dispatch/dispatch-final-tune.css'
+import './features/dispatch/dispatch-suite.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
