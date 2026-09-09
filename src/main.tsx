a09713f84e44e28c95f90dc94ba1917932c76dbc
jsd2973-tech <jsd2973@gmail.com>
2026-09-09 09:18:23 +0900

    style: enable modern dispatch PC skin

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './features/dispatch/dispatch-polish.css'
+import './features/dispatch/dispatch-modern.css'
 import App from './App.tsx'
 import DispatchAuthGate from './features/dispatch/DispatchAuthGate.tsx'
 
