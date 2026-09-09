dac952bd7db88ab2db2ae847a476bf1b8c20051a
jsd2973-tech <jsd2973@gmail.com>
2026-09-09 13:52:30 +0900

    style: remove layered dispatch overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import DispatchAuthGate from './features/dispatch/DispatchAuthGate.tsx'
-import './features/dispatch/dispatch-polish.css'
-import './features/dispatch/dispatch-modern.css'
-import './features/dispatch/dispatch-final-tune.css'
-import './features/dispatch/dispatch-suite.css'
 import './features/dispatch/dispatch-reference-final.css'
 
 createRoot(document.getElementById('root')!).render(
