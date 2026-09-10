3cf78e59d3852baed18b1314e3d25fbff4e2a9e3
jsd2973-tech <jsd2973@gmail.com>
2026-09-10 16:10:08 +0900

    fix: load card input compatibility guard

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './cardInputCompat'
 import App from './App.tsx'
 import AuthEntry from './AuthEntry.tsx'
 import DispatchAuthGate from './features/dispatch/DispatchAuthGate.tsx'
